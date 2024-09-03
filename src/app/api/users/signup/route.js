70cf27b9b094735418429adde563f8ad12c12590
atikur28 <atikur45683@gmail.com>
2024-09-03 21:07:50 +0600

    Backend code of signup and sending email implemented

---
+++ src/app/api/users/signup/route.js
@@ -0,0 +1,44 @@
+import User from "@/models/userModel";
+import { mongoDbConnect } from "@/mongoDB/mongoDB";
+import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendMail } from "@/utils/sendEmail";
+
+mongoDbConnect();
+
+export async function POST(request) {
+    try {
+        const payload = await request.json();
+        const {firstName, lastName, email, password} = payload;
+
+        const user = await User.findOne({email});
+
+        if(user) {
+            return NextResponse.json({error: "User already exists"}, {status: 400}); 
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            status: "User"
+        });
+
+        const savedUser = await newUser.save();
+
+        await sendMail({email, emailType: "VERIFY", userId: savedUser._id})
+
+        return NextResponse.json({
+            message: "User registered successfully!",
+            success: true,
+            savedUser
+        })
+
+    } catch (error) {
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}
---
+++ src/utils/sendEmail.js
@@ -0,0 +1,56 @@
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+import nodemailer from "nodemailer";
+
+export const sendMail = async ({ email, emailType, userId }) => {
+    try {
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+        if (emailType === "VERIFY") {
+            await User.findByIdAndUpdate(userId,
+                {
+                    $set: {
+                        verifyToken: hashedToken,
+                        verifyTokenExpiry: Date.now() + 3600000
+                    }
+                }
+            )
+        }
+        else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId,
+                {
+                    $set: {
+                        forgotPasswordToken: hashedToken,
+                        forgotPasswordTokenExpiry: Date.now() + 3600000
+                    }
+                }
+            )
+        }
+
+        const auth = nodemailer.createTransport({
+            service: "gmail",
+            secure: true,
+            port: 465,
+            auth: {
+                user: "imposter45683@gmail.com",
+                pass: "kbmmrhfrdqgrlius"
+            }
+        });
+
+        const mailOptions = {
+            from: 'imposter45683@gmail.com',
+            to: email,
+            subject: emailType === "VERIFY" ? "Verify your account" : "Reset your Password",
+            html: `<p>Click <a href="http://localhost:3000/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"} or copy and paste the link below in your browser.
+                  <br> http://localhost:3000/verifyemail?token=${hashedToken}
+                  </p>`,
+        }
+
+        const mailResponse = await auth.sendMail(mailOptions);
+
+        return mailResponse;
+
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
