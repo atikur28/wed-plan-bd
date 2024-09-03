4992fef77fb55fbb85e350d6f26dac9524b05b64
atikur28 <atikur45683@gmail.com>
2024-09-03 21:30:46 +0600

    Backend code of sign in implemented

---
+++ src/app/api/users/signin/route.js
@@ -0,0 +1,46 @@
+import { mongoDbConnect } from "@/mongoDB/mongoDB";
+import User from "@/models/userModel";
+import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+mongoDbConnect();
+
+export async function POST(request) {
+    try {
+        const payload = await request.json();
+        const {email, password} = payload;
+
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return NextResponse.json({error: "User does not exists!"}, {status: 400});
+        }
+
+        const validPassword = await bcryptjs.compare(password, user.password);
+
+        if(!validPassword) {
+            return NextResponse.json({error: "Password is incorrect!"}, {status: 400});
+        }
+
+        const tokenPayload = {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
+        }
+
+        const token = await jwt.sign(tokenPayload, process.env.SECRET_TOKEN, {expiresIn: "1d"});
+
+        const response = NextResponse.json({message: "Logged in successfully!", success: true});
+
+        response.cookies.set("token", token, {
+            httpOnly: true
+        });
+
+        return response;
+
+    } catch (error) {
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}
