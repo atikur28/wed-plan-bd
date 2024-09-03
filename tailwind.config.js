939dfc6bada2b2d200fe0e29b4009e1a2e4e16a3
atikur28 <atikur45683@gmail.com>
2024-09-03 19:16:37 +0600

    Font Family, Metadata and Home page edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        lora: ["var(--font-lora)"],
+        gupter: ["var(--font-gupter)"],
+      },
     },
   },
   plugins: [],
